b1ac0909f1b516e08791ea3004ce2a1300fdad20
TalentDev1128 <crypto.dev1109@gmail.com>
2023-08-04 03:04:11 -0400

    remove old codes.

--- api_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Scanners
-
-admin.site.register(Scanners)
--- api_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ScannersViews
-
-urlpatterns = [
-    path ('listItem', ScannersViews.as_view()),
-    # path ('listItem/<str:sn>', ScannersViews.as_view()),
-]
